14dcaaf4f0e3e98b243bbf5a1c3cfc9cee103ef2
Ruijie Fang <ruijief@princeton.edu>
2023-04-17 16:38:23 -0400

    recursive cleanse

---
+++ bench/cleanse-intra.sh
@@ -0,0 +1,35 @@
+cd $1;
+echo " ********** now in directory "$1" ******** "
+echo "      continue? [enter]"
+read inp
+
+for file in `ls | grep "\.c"`; do 
+  echo "-----------------"$file"--------------"
+  out=`$DUET/duet.exe -categorize $file`
+  echo $out
+  if [[ "$out" == *"Memory:    True"* ]]; then 
+    echo "memory use found; delete?"
+    read inp; 
+    echo "deleting "$file"..."
+    rm $file;
+    echo "deleting ${file%.c}.yml..."
+    rm -- "${file%.c}.yml"
+  elif [[ "$out" == *"Floats:    True"* ]]; then
+    echo "floats use found; delete?"
+    read inp;
+    echo "deleting "$file"..."
+    rm $file;
+    echo "deleting ${file%.c}.yml..."
+    rm -- "${file%.c}.yml"
+  elif [[ "$out" == *"Recursive: True"* ]]; then 
+    echo "RECURSIVE use found; delete?"
+    read inp;
+    echo "deleting "$file"..."
+    rm $file;
+    echo "deleting ${file%.c}.yml..."
+    rm -- "${file%.c}.yml"
+
+  fi
+
+done 
+
