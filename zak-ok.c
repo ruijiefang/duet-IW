17a91571df4d844d6868f59e768fb946c7412ad7
Ruijie Fang <ruijief@princeton.edu>
2023-05-22 22:46:45 -0400

    save

---
+++ zak-ok.c
@@ -0,0 +1,8 @@
+int n;
+int main() {
+  int i;
+  int j = 0;
+  for(i=0;i<n;i++){j+=2;}
+  assert(j!=1000);
+  return 0;
+}
---
+++ zak.c
@@ -0,0 +1,8 @@
+int main() {
+  int n = __VERIFIER_nondet_int();
+  int i;
+  int j = 0;
+  for(i=0;i<n;i++){j+=2;}
+  assert(j!=1000);
+  return 0;
+}
